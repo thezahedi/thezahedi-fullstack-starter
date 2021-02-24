d38e1ba020999c6d64c3eb5984b8f13c67a9d494
Saman Zahedi <isamanzahedi@gmail.com>
2021-02-24 12:09:49 +0330

    added vue i18n package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import router from "./router";
 import store from "./store";
+import i18n from "./i18n";
 import App from "./views/App";
 import Components from "./components/components";
 
@@ -19,5 +20,6 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     store,
+    i18n,
     render: h => h(App),
 }).$mount('#app');
