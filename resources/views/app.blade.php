fb6e40f6bcebfccf5e5c79b8b9451d8e6fdd78c2
Saman Zahedi <isamanzahedi@gmail.com>
2021-02-24 10:44:17 +0330

    changed asset loader to mix for hot reloading

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <title>Laravel</title>
 </head>
 <body>
 <div id="app">
 </div>
 
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
